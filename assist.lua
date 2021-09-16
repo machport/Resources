d495df02d40e3fed7b7174f813a3641fdd3413a2
machport <54745286+machport@users.noreply.github.com>
2021-09-16 07:31:37 -0700

    Update assist.lua

--- assist.lua
+++ assist.lua
@@ -8,9 +8,8 @@ mr = hookfunction(math.random,function(...)
     return mr(...)
 end)
 syn.request = function(data)
-    print(data.url)
     if data.Url:find("loadScript") then
-        return {Body="9e73eacc85ea18c0042365ad5e9148430643dedebc34d3e34c3dfd75fd2a6abc33b7b591c64a3f8c2fa096aa7200c617956cb7a4bc57f15672d50f16d305bc61"}
+        return {Body="13a0d940e9ade58b716df047bc5764aac44545bdafa4da3aaef7966853f45491ca591bb0a460e1601268c29b1143496914e00db48e88dbcb1e34f0f1b19554ff"}
     end
     if data.Url:find("xd") then
         return {Body="c59028bda721a768b0f6d9a4cb65fe9cab89e88d03d74af2b3c57b2eb646d2efcb4a073afc2d685f61389df1cd3143e4f9ae7124454a7613e3320d108dec1a16"}
